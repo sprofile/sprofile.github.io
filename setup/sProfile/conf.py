7ebac4df40fbe60b6b5f61dcd82ef7ed4af77d9d
sbstnbrstry <23e1995@outlook.com>
2014-02-26 23:08:30 -0300

    Nikola Setup

--- setup/sProfile/conf.py
+++ setup/sProfile/conf.py
@@ -385,7 +385,7 @@ COMMENT_SYSTEM = "disqus"
 # depends on what comment system you use. The default is
 # "nikolademo" which is a test account for Disqus. More information
 # is in the manual.
-COMMENT_SYSTEM_ID = "23e1995"
+COMMENT_SYSTEM_ID = "sprofile"
 
 # Enable annotations using annotateit.org?
 # If set to False, you can still enable them for individual posts and pages
